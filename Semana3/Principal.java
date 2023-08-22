850da996b628f8bb12e28431645e52387851e0d1
Italo-Yan <italoyan2004@gmail.com>
2023-08-22 19:58:56 -0300

    Testando Array

---
+++ Semana3/Principal.java
@@ -0,0 +1,13 @@
+package Semana3;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        int[] numeros = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
+
